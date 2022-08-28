be2f60464695a5c76fa9a189a271cab70fadabf7
Roman <rpol123453@gmail.com>
2022-08-28 14:07:33 +0300

    Add data files

---
+++ data.js
@@ -0,0 +1,101 @@
+const contacts = [
+  {
+    name: "Mark",
+    midname: "Johnson",
+    surname: "Cook",
+    imgRef:
+      "https://i.pinimg.com/236x/81/26/00/81260052e859d9fe17f80959a0449d65.jpg",
+  },
+  {
+    name: "Dexter",
+    midname: "Tiewon",
+    surname: "Gore",
+    imgRef:
+      "https://bookingagentinfo.com/wp-content/uploads/2016/07/Famous-Dex-Contact-Information.jpg",
+  },
+  {
+    name: "Drake",
+    midname: "Whatson",
+    surname: "Womack",
+    imgRef:
+      "https://pbs.twimg.com/profile_images/1092535155820965888/xEtUUowt_400x400.jpg",
+  },
+  {
+    name: "James",
+    midname: "Johnson",
+    surname: "Gates",
+    imgRef: "https://focma.com/laika/img/team/team-1.jpg",
+  },
+  {
+    name: "Tracy",
+    midname: "Troy",
+    surname: "McQueen",
+    imgRef:
+      "https://pbs.twimg.com/profile_images/1343951413966237696/pBIwyh9T_400x400.jpg",
+  },
+  {
+    name: "Jeezy",
+    midname: "Peterson",
+    surname: "Wick",
+    imgRef:
+      "https://sun1-54.userapi.com/s/v1/if1/qQkifUvZYhHeoRsivlC89POndzBwc1XB21ZqjO0b6qJ4UW4stiYj1g-1v-EOb0T4Jwd4Mg.jpg?size=400x400&quality=96&crop=343,1,1361,1361&ava=1",
+  },
+  {
+    name: "Diana",
+    midname: "Rims",
+    surname: "Trunt",
+    imgRef:
+      "https://pbs.twimg.com/profile_images/1265435469870964738/ohYXbTA8_400x400.jpg",
+  },
+  {
+    name: "Jeremy",
+    midname: "Charles Robert",
+    surname: "Clarkson",
+    imgRef:
+      "https://sun1-15.userapi.com/impg/4CyLg15C7NZJvLIWn20BQMGH7UxloPo_jEDJyQ/-XnQ84VuwmU.jpg?size=400x0&quality=96&crop=0,250,999,999&sign=f890133a6d79ff1218845f90046f4396&c_uniq_tag=yLeL9KoSzQAUEykEP11VyDtFxvS4Kt1En2UQg7dIyCc&ava=1",
+  },
+  {
+    name: "Symere",
+    midname: "Bysil",
+    surname: "Woods",
+    imgRef:
+      "https://pbs.twimg.com/profile_images/1294078788654030853/sOKV1CYA.jpg",
+  },
+  {
+    name: "Dimitri",
+    midname: "Leslie",
+    surname: "Roger",
+    imgRef:
+      "https://yt3.ggpht.com/a/AGF-l7_UKyO3koyEpHTDJxmV4CCNC6KcJ-q6LDBFjA=s400-c-k-c0xffffffff-no-rj-mo",
+  },
+  {
+    name: "Vladimir",
+    midname: "Rudolfovich",
+    surname: "Lubovniy",
+    imgRef:
+      "https://www.modelsea.com/wp-content/uploads/2019/07/dj-vlad-email-phone-contact-457.jpg",
+  },
+  {
+    name: "Adam",
+    midname: "John",
+    surname: "Grandmaison",
+    imgRef:
+      "https://c8d8q6i8.stackpathcdn.com/wp-content/uploads/2018/09/Adam-22-Grandmaison-Contact-Information.png",
+  },
+  {
+    name: "Travis",
+    midname: "Landon",
+    surname: "Barker",
+    imgRef:
+      "https://informationcradle.com/wp-content/uploads/2019/08/Travis-Barker-Photo-1-400x400.jpg",
+  },
+  {
+    name: "Kourtney",
+    midname: "Mary",
+    surname: "Kardashian",
+    imgRef:
+      "https://pbs.twimg.com/profile_images/486553268383129600/xLkkvXAb_400x400.jpeg",
+  },
+];
+
+module.exports = contacts;
